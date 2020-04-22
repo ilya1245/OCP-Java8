8a13924e2c7620e633ee964a28143008eaa677a6
Ilya Miflig <Ilya_Miflig@epam.com>
2020-04-22 08:23:15 +0300

    Add predicate samples

---
+++ src/main/java/com/midway/ocp8/lambda/PredicateSample.java
@@ -0,0 +1,18 @@
+package com.midway.ocp8.lambda;
+
+import com.midway.ocp8.lambda.model.Dog;
+import java.util.function.Predicate;
+
+public class PredicateSample {
+
+  public static void main(String[] args) {
+    Dog boi = new Dog(9, "boi", 30);
+    Dog clover = new Dog(10, "clover", 25);
+    Predicate<Dog> p = d -> d.getAge() > 9;
+    Predicate<Dog> p2 = d -> d.getAge() < 4;
+
+    System.out.println("Is Boi older than 9 - " + p.test(boi));
+    System.out.println("Is Clover older than 9 - " + p.test(clover));
+  }
+
+}
